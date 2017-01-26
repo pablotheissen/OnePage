f0e9041e1c5b23e6ed3c4919320ebaf7b10adffe
Pablo Theissen <pt+github@pablo-t.de>
2017-01-26 09:49:29 +0100

    Remove unnecessary code for changing icon

--- common.js
+++ common.js
@@ -7,20 +7,3 @@ function cleanUrl(url) {
     }
     return url;
 }
-
-// Takes a local path to intended 19x19 icon
-//   and passes a correct SetIcon action to the callback
-function createSetIconAction(path, callback) {
-  var canvas = document.createElement("canvas");
-  var ctx = canvas.getContext("2d");
-  var image = new Image();
-  image.onload = function() {
-    ctx.drawImage(image,0,0,19,19);
-    var imageData = ctx.getImageData(0,0,19,19);
-    var action = new chrome.declarativeContent.SetIcon({imageData: imageData});
-    callback(action);
-  }
-  image.src = chrome.runtime.getURL(path);
-}
-
-createSetIconAction("what.png");
