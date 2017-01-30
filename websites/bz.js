17576b1b6743afa6cb3f46c1f1c05f92e3d96edf
Pablo Theissen <pt+github@pablo-t.de>
2017-01-30 10:58:49 +0100

    Simplified BZ script with querySelector() function

--- websites/bz.js
+++ websites/bz.js
@@ -2,25 +2,24 @@
   * http://www.berliner-zeitung.de/berlin/stadtratskandidat-in-neukoelln-schwul--migranten-lehrer--afd-politiker-25608022
   */
 /* article is fully accessible on site but hidden as printonly */
-var textContainer = document.getElementsByClassName("dm_article_text")[0];
-var printContainer = textContainer.getElementsByClassName("printonly")[0];
-
+var textContainer = document.querySelector(".dm_article_text");
+var printContainer = textContainer.querySelector(".printonly");
 
 /* we'll fetch each element hidden by printonly and append it to the non-hidden container */
-printContainer.childNodes.forEach(function (element, index, array) {
+printContainer.childNodes.forEach(function (element) {
     textContainer.appendChild(element.cloneNode(true));
 });
 
 /* clean up pagination and printonly area, as it is not needed anymore */
 printContainer.remove();
-document.getElementsByClassName("dm_table_of_contents_wrapper")[0].remove();
-document.getElementsByClassName("dm_pagination_simple")[0].remove();
+document.querySelector(".dm_table_of_contents_wrapper").remove();
+document.querySelector(".dm_pagination_simple").remove();
 
 /* convert <p class="article_pagebreak"> to <h3> */
-var headings_old = document.getElementsByClassName("article_pagebreak");
-[].forEach.call(headings_old, function (element, index, array) { /* http://stackoverflow.com/a/3871602 */
+var headings_old = document.querySelectorAll(".article_pagebreak");
+headings_old.forEach(function (heading_old) {
     var heading_new = document.createElement("h3");
-    heading_new.innerHTML = element.innerHTML;
-    element.parentElement.replaceChild(heading_new, element);
+    heading_new.innerHTML = heading_old.innerHTML;
+    heading_old.parentElement.replaceChild(heading_new, heading_old);
     heading_new = null;
-});
+})
