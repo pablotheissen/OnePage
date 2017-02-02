ed4d0375c5040a92ce73fa8f348a3973b57c4d80
Pablo Theissen <pt+github@pablo-t.de>
2017-02-02 13:49:44 +0100

    Support for loading content from different URL
    Created function for extracting content from a different URL more
    easily, minimizing the footprint in websites/*.js

--- common.js
+++ common.js
@@ -1,3 +1,6 @@
+/**
+ * Strip URL from parameters like ? and #
+ */
 cleanUrl = function (url) {
     if (url.indexOf("#") != -1) {
         url = url.substring(0, url.indexOf("#"));
@@ -8,15 +11,31 @@ cleanUrl = function (url) {
     return url;
 }
 
-/* TODO: combine getDomByUrl and getContentByUrl */
-getDomByUrl = function (url, callfunction) {
+/**
+ * url: URL to call
+ * getQuery: query-selector on next page whos children will be extracted
+ * appendQuery: query-selector on current page in which the new content will be appended
+ * asyncCallback: function to call when finished with extracting and appending content
+ */
+loadPageIntoDom = function (url, getQuery, appendQuery, asyncCallback) {
+    getDomByUrl(url, _loadPageIntoDom, {getQuery: getQuery, appendQuery: appendQuery, asyncCallback: asyncCallback});
+}
+_loadPageIntoDom = function (dom, asyncPass) {
+    dom.querySelectorAll(asyncPass["getQuery"]).forEach(function (element) {
+        document.querySelector(asyncPass["appendQuery"]).appendChild(element);
+    });
+    if (asyncPass["asyncCallback"]) {
+        asyncPass["asyncCallback"]();
+    }
+}
+
+getDomByUrl = function (url, asyncCallback, asyncPass) {
     var xhr = new XMLHttpRequest();
+
     xhr.onreadystatechange = function () {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            if (xhr.status == 200 || xhr.status == 0) {
-                var parser = new DOMParser();
-                var doc = parser.parseFromString(xhr.responseText, "text/html");
-                callfunction(doc);
+            if (xhr.status == 200) {
+                asyncCallback(htmlToDom(xhr.responseText), asyncPass);
                 return true;
             }
             else {
@@ -26,27 +45,17 @@ getDomByUrl = function (url, callfunction) {
             }
         }
     }
-    xhr.open("GET", url, true);
+        
+    xhr.open("GET", url);
     xhr.send();
 }
 
-getContentByUrl = function (url, callfunction) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            if (xhr.status == 200 || xhr.status == 0) {
-                callfunction(xhr.responseText);
-                return true;
-            }
-            else {
-                console.log("Error while loading " + url + ".\n");
-                console.log(xhr.status);
-                return false;
-            }
-        }
-    }
-    xhr.open("GET", url, true);
-    xhr.send();
+/**
+ * Convert HTML sourcecode to DOM
+ */
+htmlToDom = function (htmlSourceCode) {
+    var parser = new DOMParser();
+    return parser.parseFromString(htmlSourceCode, "text/html");
 }
 
 var sending = chrome.runtime.sendMessage({ cmd: "showActiveIcon" });
